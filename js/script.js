d16f86bf4631108dd6197d9d3b9fc410aa2ed20c
lnalmeida <l.n.almeida.ti@gmail.com>
2022-03-23 01:19:00 -0300

    added auto play button, sstill not implemented

--- js/script.js
+++ js/script.js
@@ -1,8 +1,41 @@
 window.addEventListener('DOMContentLoaded', function() {
     const button = document.querySelector('.button');
     const colorText = document.querySelector('.color-display span');
+    const switchButton = document.querySelector('.label');
+    
+    let switchIsOn = false;
+
+    // switchButton.addEventListener('click', function() {
+    //     if (switchIsOn) {
+    //         console.log(switchIsOn);
+    //         switchIsOn = false;
+    //     } 
+    //     if(!switchIsOn) {
+    //         console.log(switchIsOn)
+    //         switchIsOn = true;
+    //     }
+    // })
     
     button.addEventListener('click', () => {
+        getRandomColor()
+    })
+
+    const autoPlayOn = () => {
+            const playInterval = setInterval(() => {
+                getRandomColor();
+            }, 2000)
+            this.classList.remove('off');
+            this.classList.add('on');
+        }
+
+    const autoPlayOff = () => {
+            clearInterval(playInterval);
+            this.classList.remove('on');
+            this.classList.add('off');
+    }
+    
+
+    const getRandomColor = () => {
         let str = '#';
         while (str.length < 7) {
             str += Math.floor(Math.random() * 0x10).toString(16);
@@ -10,5 +43,5 @@ window.addEventListener('DOMContentLoaded', function() {
         document.body.style.backgroundColor = str;
         colorText.textContent = str;
         colorText.style.color = str;
-    })
+    }
 })
